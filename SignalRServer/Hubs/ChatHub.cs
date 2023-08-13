385ad7f5a891f2f07d21d0847467db5152c0c913
tedead <jhalv27@gmail.com>
2023-08-13 15:57:51 -0500

    Add project files.

---
+++ SignalRServer/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServer.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        private readonly IHubContext<ChatHub> _hub;
+
+        public Task SendNotificationAsync(string message)
+        {
+            return _hub.Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
